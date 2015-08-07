4aafd69757ecf265518939c745a4067f2b7b9ba2
frozenpixels <jess@frozenpixelstudios.com>
2015-08-06 23:58:57 -0400

    fibonacci programs

--- tourCh3Fibonacci.go
+++ tourCh3Fibonacci.go
@@ -1,14 +1,26 @@
+//The fibonacci series is:  x(n+2)=x(n+1)+x(n),
+//where x(1) = 1 and x(0) = 0
 package main
 
 import "fmt"
 
 //fibonacci is a funtion that returns
 //a function that returns an int
-func fibonacci() func() int {
+func fibonacci() func() int64 {
+  fmt.Println("fib called")
+  a := int64(1)
+  b := int64(0)
+
+  return func() int64 {
+  	fmt.Println("fib recur called")
+    a, b = b, a+b
+    return a
+  }
 }
 
 func main() {
   f := fibonacci()
+
   for i:=0; i<10; i++ {
     fmt.Println(f())
   }
---
+++ tourCh3Fibonacci2.go
@@ -0,0 +1,24 @@
+//The fibonacci series is:  x(n+2)=x(n+1)+x(n),
+//where x(1) = 1 and x(0) = 0
+package main
+
+import "fmt"
+
+//fibonacci is a funtion that returns
+//a function that returns an int
+func fibonacci(x int64) int64 {
+  if x == 0 {
+    return 0
+  } else if x == 1 {
+    return 1
+  } else {
+    return fibonacci(x-1) + fibonacci(x-2)
+  }
+}
+
+func main() {
+  for i:=0; i<60; i++ {
+    fmt.Println("i = ", i, " ", fibonacci(int64(i)))
+  }
+}
+
