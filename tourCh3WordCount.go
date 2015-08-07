e37cb17fa81e3d4d65f5f09d9ebd691f84651875
Jess Sturgeon <jsturgeon@ispatechnology.com>
2015-08-07 14:53:05 -0400

    2 exercies

--- tourCh3WordCount.go
+++ tourCh3WordCount.go
@@ -6,16 +6,30 @@
 
 package main
 
-include (
-  "fmt"
-  "golang.org/x/tour/wc"
+import (
+	_ "fmt"
+	"strings"
+
+	"golang.org/x/tour/wc"
 )
 
 func WordCount(s string) map[string]int {
-  return map[string]int{"x" : 1 }
+
+	sMap := make(map[string]int) //or:  var sMap map[string]int
+	var sArray []string = strings.Fields(s)
+
+	for _, iStr := range sArray {
+		elem, ok := sMap[iStr]
+		if ok {
+			sMap[iStr] = elem + 1
+		} else {
+			sMap[iStr] = 1
+		}
+	}
+
+	return sMap
 }
 
 func main() {
-  wc.Test(WordCount)
+	wc.Test(WordCount)
 }
-
